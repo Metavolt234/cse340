7c97d0c9a9c798519979a0c5acea48cab34b37d3
Metavolt234 <metavolt3@gmail.com>
2026-07-11 16:01:04 +0300

    week 02

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -41,8 +43,10 @@ app.get('/', async (req, res) => {
 
 // Organizations Page
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+
+    res.render('organizations', { title, organizations });
 });
 
 // Projects Page
@@ -61,7 +65,12 @@ app.get('/categories', async (req, res) => {
 /**
  * Start the server
  */
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
 });
